b154933b31f0ae2db0ed347bfb31a2a7b15b4694
kicki58 <kristina.martinelle@arkeologi.uu.se>
2023-05-31 14:00:30 +0200

    massor med SAUscript

--- teknisk_o_system_attr.sql
+++ teknisk_o_system_attr.sql
@@ -8,3 +8,14 @@ where "ObjectId"=3 and a."AttributeId"=av."AttributeId";
 SELECT  a."Label",av."Value"
 FROM public."Attribute" as a, public."AttributeValue" as av 
 where "ObjectId"=1 and a."AttributeId"=av."AttributeId";
+
+koordinatsystem
+SELECT  a."Label",av."Value", a."MetaId", a."ObjectId",a."AttributeId"
+FROM public."Attribute" as a, public."AttributeValue" as av 
+where "MetaId"=489 and a."AttributeId"=av."AttributeId";
+
+sätta koordinatsystem
+update "AttributeValue" as av
+  set "Value" = '3006'
+from public."Attribute" as a
+where "MetaId"=489 and a."AttributeId"=av."AttributeId";
